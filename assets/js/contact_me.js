dbfaa5f8d1591027877c62437ecce3acdc79dff0
José Manuel Sánchez <josemas@gmail.com>
2024-03-14 09:35:57 +0100

    Update contact_me.js

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -40,7 +40,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Tu mensaje se ha enviado correctamente y te contestará en breve. </strong>");
+            .append("<strong>Tu mensaje se ha enviado correctamente y te contestaré en breve. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
