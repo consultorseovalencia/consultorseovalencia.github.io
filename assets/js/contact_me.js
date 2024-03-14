98791cadf105aba1bb3908676dbd49091204a9ec
José Manuel Sánchez <josemas@gmail.com>
2024-03-14 09:33:19 +0100

    Update contact_me.js

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -40,7 +40,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Tu mensaje se ha enviado correctamente y te contestará en breve. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -52,7 +52,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", parece que tenemos problemas para enviar tu mensaje, prueba de nuevo en unos minutos, por favor."));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
